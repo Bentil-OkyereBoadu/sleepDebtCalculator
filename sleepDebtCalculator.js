11f70089ff8e5bbcfd4c004a7198e97acd744909
Bentil-OkyereBoadu <91896057+Bentil-OkyereBoadu@users.noreply.github.com>
2021-10-06 15:17:59 +0000

    Update sleepDebtCalculator.js

--- sleepDebtCalculator.js
+++ sleepDebtCalculator.js
@@ -41,16 +41,3 @@ const getSleepHours = day => {
   calculateSleepDebt();
   
 
-  /**
-   * 2.
-For extra practice, try these:
-
-getActualSleepHours() could be implemented without calling getSleepHours(). 
-Use literal numbers and the + operator to rewrite getActualSleepHours(). 
-It should still return the total actual hours slept in the week.
-
-Some people need to sleep longer than others. 
-Rewrite getIdealSleepHours() so that you can pass it an argument,
- like getIdealSleepHours(8) where 8 is the ideal hours per night. 
- Update the call to getIdealSleepHours() in calculateSleepDebt() too.
-   */
